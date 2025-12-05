1171d4f22d82d345aaf6ee6701344ecd2c86892e
junaid <mohmmadjunaidqurashi@gmail.com>
2025-12-05 16:14:46 +0530

    react-bootstrap

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,24 +2,17 @@ import './css/style.css';
 import "./App.css";
 import styled from 'styled-components';
 import Child from './child';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+
 
 
 function App() {
-  const Heading = styled.h3 `color:red; border:1px solid #ddd; `
-  const Btn = styled.button `
-  pdding: 10px;
-  border:none;
-  background:red;
-  color:white;
-  cursor:pointer;
-  margin:10px;
-  width:100px;
-  height100px;
-  `
+ 
  return <>
 
-<Heading>Hello</Heading>
-<Btn>Login</Btn>
+<Button>Sign</Button> 
+<Alert>Hello Bs Installed</Alert>
  </>
 
   }
