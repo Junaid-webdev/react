305e0059668f945453f2dab8452c9e80a46f7a09
junaid <mohmmadjunaidqurashi@gmail.com>
2025-12-05 17:03:20 +0530

    useRef

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,15 +4,37 @@ import styled from 'styled-components';
 import Child from './child';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
+import { useRef } from 'react';
 
 
 
 function App() {
- 
+  const inputhandler = ()=>{
+    console.log(inputController); 
+    inputController.current.focus();
+    inputController.current.style.color='red';
+    
+  }
+  const h1Ref = useRef(null);
+  const inputController=useRef(null);
+  const togglehandler = ()=>{
+  if(  inputController.current.style.display!='none'){
+    inputController.current.style.display='none'
+  }else{
+    inputController.current.style.display='inline'
+  }
+  }
+  const h1handler = ()=>{
+    h1Ref.current.style.color='yellow';
+  }
  return <>
+ <h3>Useref</h3>
+ <button onClick={togglehandler}>Toggle</button>
+ <input ref={inputController} type="text" placeholder='Enter Your Name' />
+ <button onClick={inputhandler}>Click</button>
 
-<Button>Sign</Button> 
-<Alert>Hello Bs Installed</Alert>
+ <h1 ref={h1Ref}>Hello</h1>
+ <button onClick={h1handler}>Handler</button>
  </>
 
   }
