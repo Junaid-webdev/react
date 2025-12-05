9a43ddaf3fa3c26e35dbf9e54eb3ff2d7e83b172
junaid <mohmmadjunaidqurashi@gmail.com>
2025-12-05 12:54:43 +0530

    css modules

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,139 +1,26 @@
 import './css/style.css';
 import "./App.css";
+import Child from './child';
 
 
 function App() {
  return <>
- 
- <h1 className='heading'>External Css</h1>
- <div className='container'>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- <div className='user-card'> 
-  <div>
-    <img className='img-style' src="https://png.pngtree.com/png-clipart/20250418/original/pngtree-cartoon-cute-little-boy-student-giving-isolated-with-transparent-background-png-image_20720809.png" alt="" />
-  </div>
-  <div className='text-wrap'>
-    <h4>Sameer</h4>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
- </div>
- </div>
+<h1>Style With css Modules in React js</h1>
+<div style={{ display:"flex",flexWrap:"wrap" }}>
+<Child/>
+<Child/>
+<Child/>
+<Child/>
+<Child/>
+<Child/>
+<Child/>
+<Child/>
+<Child/>
+<Child/>
+<Child/>
+<Child/>
+<Child/>
+</div>
  </>
 
   }
---
+++ react-app/src/child.jsx
@@ -0,0 +1,24 @@
+import style from './css/child.module.css';
+
+function Child(){
+
+
+
+    return<>
+    
+    <div>
+     
+        <div className={style.card}>
+            <img className={style.img} src="https://png.pngtree.com/png-clipart/20221017/original/pngtree-kid-walking-to-school-hand-swinging-png-image_8699106.png" alt="" />
+       
+        <div className={style.textWrap}>
+            <h4>Sameer</h4>
+            <p>Lorem ipsum dolor sit.</p>
+        </div>
+    </div>
+    </div>
+    
+    
+    </>
+}
+export default Child;
