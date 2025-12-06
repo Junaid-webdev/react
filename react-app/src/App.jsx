1df1da8490c61bbc46f62aad3fe5168dd65cbee8
junaid <mohmmadjunaidqurashi@gmail.com>
2025-12-06 18:17:36 +0530

    Fragment react

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,38 +1,14 @@
-import { useId } from "react";
+import { Fragment } from "react";
 
 
 function App(){
 
+
   return <>
-  <h1>useId Hook</h1>
-  <User/>
-  <User/>
- 
-  </>
-}
-
-
-function User(){
-const name = useId();
-const password = useId();
-const terms = useId();
-const skills = useId();
-  return <>
-  
-  <form action="">
-    <label htmlFor={name}>Enter User Name:  </label>
-      <input type="text" id={name} placeholder="Enter Your Name" />
-      <br /><br />
-      <label htmlFor={password}>Enter Password </label>
-      <input type="text" id={password} placeholder="Enter Password" />
-      <br /><br />
-      <label htmlFor={terms}>Enter Trems </label>
-      <input type="text" id={terms} placeholder="Enter trems" />
-      <br /><br />
-      <label htmlFor={skills}>Trems and Condition  </label>
-      <input type="checkbox" id={skills} placeholder="Enter skills" />
-  </form>
-
+ <Fragment>
+  <h2>Fragment in React js</h2>
+  <h2>Fragment in React js</h2>
+</Fragment>
   </>
 }
 export default App;
